ac3d7c8c9b636ee66c88261bde10152d65a8d82a
dh-k22q <>
2023-04-20 14:21:46 +0900

    feat-생성

---
+++ src/main/java/com/omok/nyang/NyangApplication.java
@@ -0,0 +1,13 @@
+package com.omok.nyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyangApplication.class, args);
+	}
+
+}
